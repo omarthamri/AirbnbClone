59eac0879965469dd994cffb2eedceca08cec264
omar thamri <omarthamri4@gmail.com>
2023-10-21 20:58:53 +0100

    implement explore view ui

--- AirbnbClone/Core/Listing/View/ListingItemView.swift
+++ AirbnbClone/Core/Listing/View/ListingItemView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    var images = ["listing1","listing2","listing3","listing4"]
     var body: some View {
         VStack(spacing: 8) {
             // images
-             Rectangle()
-                .frame(height: 320)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+            TabView {
+                ForEach(images,id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
             // listing details
             HStack(alignment: .top) {
                 // details
@@ -39,7 +47,6 @@ struct ListingItemView: View {
             }
             .font(.footnote)
         }
-        .padding()
     }
 }
 
