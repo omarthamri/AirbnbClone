436fa45eb0dc4adf3101d6a087b015af0f073e57
omar thamri <omarthamri4@gmail.com>
2023-10-21 17:38:44 +0100

    first commit

---
+++ AirbnbClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbClone
+//
+//  Created by omar thamri on 21/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
