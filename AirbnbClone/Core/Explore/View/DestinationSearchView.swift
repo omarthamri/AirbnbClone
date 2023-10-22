1f14773c5050c94fe10a51ce74a4431b1d4e6232
omar thamri <omarthamri4@gmail.com>
2023-10-22 18:31:43 +0100

    implement destination search view

---
+++ AirbnbClone/Core/Explore/View/DestinationSearchView.swift
@@ -0,0 +1,127 @@
+//
+//  DestinationSearchView.swift
+//  AirbnbClone
+//
+//  Created by omar thamri on 22/10/2023.
+//
+
+import SwiftUI
+
+enum DestinationSearchOptions {
+    case location
+    case dates
+    case guests
+}
+
+struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State private var destination: String = ""
+    @State private var selectedOption: DestinationSearchOptions = .location
+    var body: some View {
+        VStack() {
+            Button(action: {
+                withAnimation(.snappy) {
+                    show.toggle()
+                }
+            }, label: {
+                Image(systemName: "xmark.circle")
+                    .imageScale(.large)
+                    .foregroundStyle(.black)
+            })
+            VStack(alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Searching destinations", text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    CollapsedPickerView(title: "Where", description: "Add destination")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .location ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                selectedOption = .location
+            }
+            // date selection view
+            VStack {
+                if selectedOption == .dates {
+                    HStack {
+                        Text("Show expanded view")
+                        Spacer()
+                    }
+                } else {
+                    CollapsedPickerView(title: "When", description: "Add dates")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .dates ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                    selectedOption = .dates
+                }
+            // num guests view
+            VStack {
+                if selectedOption == .guests {
+                    HStack {
+                        Text("Show expanded view")
+                        Spacer()
+                    }
+                } else {
+                    CollapsedPickerView(title: "Who", description: "Add guests")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .guests ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                selectedOption = .guests
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    DestinationSearchView(show: .constant(false))
+}
+
+struct CollapsedPickerView: View {
+    let title: String
+    let description: String
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                Spacer()
+                Text(description)
+                
+            }
+            .font(.subheadline)
+            .fontWeight(.semibold)
+        }
+    }
+}
--- AirbnbClone/Core/Explore/View/ExploreView.swift
+++ AirbnbClone/Core/Explore/View/ExploreView.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var showSearchDestinationView = false
     var body: some View {
+        
         NavigationStack {
-            ScrollView {
-                SearchAndFilterBar()
-                LazyVStack(spacing: 32) {
-                        ForEach(0..<10,id: \.self) { listing in
-                            NavigationLink(value: listing) {
-                                ListingItemView()
-                                    .frame(height:400)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            if showSearchDestinationView {
+                DestinationSearchView(show: $showSearchDestinationView)
+            } else {
+                ScrollView {
+                    SearchAndFilterBar()
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                showSearchDestinationView.toggle()
                             }
                         }
+                    LazyVStack(spacing: 32) {
+                            ForEach(0..<10,id: \.self) { listing in
+                                NavigationLink(value: listing) {
+                                    ListingItemView()
+                                        .frame(height:400)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                }
+                            }
+                    }
+                    .padding()
                 }
-                .padding()
-            }
-            .navigationDestination(for: Int.self) { listing in
-                ListingDetailView()
-                    .navigationBarBackButtonHidden()
+                .navigationDestination(for: Int.self) { listing in
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
+                }
             }
         }
     }
